abe582c10843db112e7366f40e679ff17295c1fd
victorOyeks <victor.oyekanmi@finopay.com>
2025-12-11 16:18:49 +0100

    updated

---
+++ src/LinkedList.java
@@ -0,0 +1,7 @@
+public class LinkedList {
+
+    /*
+    Linked list is a form of sequential collection, and it does not have to be in order. A linked list is made up of
+    independent nodes that may contain any type of data and each node has a reference to the next node in the link.
+     */
+}
